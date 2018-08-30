f0b4930ce739a5a3384bc39bbee137c2bb1f1288
Erik H. Beck <beck.erik@epa.gov>
2018-08-30 08:42:56 -0400

    config files

---
+++ ParkingLot/set.mypath.bat
@@ -0,0 +1,5 @@
+REM Update R path, Emacs Path, and add Rtools to the path
+REM 5 January 2017
+
+set PATH=%path%;C:\Users\EBECK\Emacs-25.3\bin;C:\Users\EBECK\AppData\Local\Pandoc;C:\Users\EBECK\MikTeX\miktex\bin\x64;C:\Program Files\R\R-3.4.3\bin\x64
+
