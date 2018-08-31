41d2a67d717659d3ca248cbd91a33c121b17314c
Erik H. Beck <beck.erik@epa.gov>
2018-08-31 09:21:40 -0400

    Adding a basic R test script

---
+++ ParkingLot/TestChug.R
@@ -0,0 +1,10 @@
+# R Script to Test R Install and load packages relevant for the instruction session.
+# Erik Beck, 8/31/2018
+
+Sys.getenv()
+update.packages()
+require("rmarkdown")
+require("tidyverse")
+require("knitr")
+require("installr")
+require("lubridate")
